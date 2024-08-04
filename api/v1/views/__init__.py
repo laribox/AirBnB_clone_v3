8e680e3e413a22290b2fed5f347f91a42b874279
laribox <laribox2014@gmail.com>
2024-08-04 16:18:50 +0000

    task 6 state crud api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Wildcard import (PEP8 will complain, but it's okay for this specific case)
 from api.v1.views.index import *
+from api.v1.views.states import *
