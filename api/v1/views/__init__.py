dfbbbb3b0fc72d063a701e648c6deafaf5dc628b
laribox <laribox2014@gmail.com>
2024-08-04 17:46:50 +0000

    create crud api for cities

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,9 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Wildcard import (PEP8 will complain, but it's okay for this specific case)
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
