2bbd7dad4819f006d5d852d159621d3fed1a3da8
laribox <laribox2014@gmail.com>
2024-08-04 07:24:12 -0700

    task 2 adding get and count functions

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,18 +74,25 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
     def get(self, cls, id):
         """
-        Returns the object based on the class name and its ID, or None if not found.
+        Returns the object based on the class name and its ID, or None if not
+        found
         """
-        return self.__session.query(classes[cls]).filter_by(id=id).first()
+        objects = self.__session.query(classes[cls])
+        for obj in objects:
+            if obj.id == id:
+                return obj
+        return None
 
     def count(self, cls=None):
         """
         Returns the number of objects in storage matching the given class name.
         If no name is passed, returns the count of all objects in storage.
         """
-        if cls:
-            return self.__session.query(classes[cls]).count()
-        else:
-            return sum(self.__session.query(c).count() for c in classes.values())
+        nobjects = 0
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                nobjects += len(self.__session.query(classes[clss]).all())
+        return nobjects
