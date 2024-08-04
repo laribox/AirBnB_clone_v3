0b1f367b1014cc6423f0ebd4e3f6b7c1d54e385f
laribox <laribox2014@gmail.com>
2024-08-04 17:07:14 +0000

    fixed the class doesnt get the name used __name__

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,7 +16,7 @@ def get_states():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object"""
-    state = storage.get(State, state_id)
+    state = storage.get(State.__name__, state_id)
     if not state:
         abort(404)
     return jsonify(state.to_dict())
@@ -26,7 +26,7 @@ def get_state(state_id):
                  methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object"""
-    state = storage.get(State, state_id)
+    state = storage.get(State.__name__, state_id)
     if not state:
         abort(404)
     storage.delete(state)
@@ -51,7 +51,7 @@ def create_state():
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Updates a State object"""
-    state = storage.get(State, state_id)
+    state = storage.get(State.__name__, state_id)
     if not state:
         abort(404)
     if not request.json:
