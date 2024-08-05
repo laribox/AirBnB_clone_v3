b8135dd992f195db5e375870f5f2abb69cf925f3
laribox <laribox2014@gmail.com>
2024-08-05 23:50:06 +0000

    fixing pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 """ Init file """
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Wildcard import (PEP8 will complain, but it's okay for this specific case)
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -13,3 +8,7 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
